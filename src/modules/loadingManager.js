39436912a5e811d343c66e31195659d6c8a81962
metaeavies <gordon@metaheavies.com>
2023-04-23 09:38:08 +0700

    Show loaded assets

--- src/modules/loadingManager.js
+++ src/modules/loadingManager.js
@@ -4,27 +4,29 @@ export function createLoadingManager() {
 
     const loadingManager = new THREE.LoadingManager()
 
+    const messageDiv = document.querySelector('.loading-message')
     const progressBarWrap = document.querySelector('.progress-bar-wrap')
     const progressBar = document.getElementById('progress-bar')
 
     const isLoaded = () => progressBarWrap.classList.contains('loaded')
 
 
-    loadingManager.onStart = function (url, item, total) {
-        console.log(`Started loading ${url}`)
+    loadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
+        console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
     }
 
-    loadingManager.onProgress = function (url, loaded, total) {
-        progressBar.value = (loaded / total) * 100    
+    loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+        progressBar.value = (itemsLoaded / itemsTotal) * 100    
+        messageDiv.innerHTML = 'Loading ' + url + '<br>File ' + itemsLoaded + ' of ' + itemsTotal
     }
 
     loadingManager.onLoad = function () { 
         progressBarWrap.classList.add('loaded')
-
         progressBar.style.display = "none"
+        messageDiv.style.display = "none"
 
         setTimeout(() => {
-            progressBarWrap.parentNode.removeChild(progressBarWrap);
+            progressBarWrap.parentNode.removeChild(progressBarWrap)
         }, 4000);           
     }
 
